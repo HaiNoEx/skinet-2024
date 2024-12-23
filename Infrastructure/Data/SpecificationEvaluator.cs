bc29651832bf3e18adbaa6eaa7768c367eee80b0
Hai Vu Duc <vhai2003@gmail.com>
2024-12-24 01:44:16 +0700

    End of Section 5

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -23,11 +23,15 @@ public class SpecificationEvaluator<T> where T : BaseEntity
             query = query.Distinct();
         }
 
+        if(spec.IsPagingEnabled){
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
+
         return query;
     }
 
     public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query,
-     ISpecification<T, TResult> spec){
+    ISpecification<T, TResult> spec){
         if(spec.Criteria != null){
             query = query.Where(spec.Criteria);
         }
@@ -48,6 +52,10 @@ public class SpecificationEvaluator<T> where T : BaseEntity
 
         if(spec.IsDistinct){
             selectQuery = selectQuery?.Distinct();
+        }
+
+        if(spec.IsPagingEnabled){
+            selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
         }
 
         return selectQuery ?? query.Cast<TResult>();
