f964e62b0afe0c1dccb22bc83a1e1b7c4dc74598
Hai Vu Duc <vhai2003@gmail.com>
2025-01-31 15:00:28 +0700

    End of section 9

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -10,7 +10,7 @@ public class ProductSpecification: BaseSpecification<Product>
     public ProductSpecification(ProductSpecParams specParams) : base(x => 
         (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
         (!specParams.Brands.Any() || specParams.Brands.Contains(x.Brand)) &&
-        (!specParams.Types.Any() || specParams.Brands.Contains(x.Type)) 
+        (!specParams.Types.Any() || specParams.Types.Contains(x.Type)) 
     )
     {
         ApplyPaging(specParams.PageSize *(specParams.PageIndex - 1), specParams.PageSize);
--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -1,12 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/product';
-import {MatCard } from '@angular/material/card'
+import {MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
 import {MatDialog} from '@angular/material/dialog';
 import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { ShopParams } from '../../shared/models/shopParams';
+import {MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-shop',
@@ -14,7 +22,13 @@ import { MatIcon } from '@angular/material/icon';
     MatCard,
     ProductItemComponent,
     MatButton,
-    MatIcon
+    MatIcon,
+    MatMenu,
+    MatSelectionList,
+    MatListOption,
+    MatMenuTrigger,
+    MatPaginator,
+    FormsModule
 ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
@@ -23,10 +37,19 @@ export class ShopComponent implements OnInit {
   private shopService = inject(ShopService);
   private dialogService = inject(MatDialog)
 
-  products:Product[] = [];
-  selectedBrands: string[] = []
-  selectedTypes: string[] = []
-  
+  products?: Pagination<Product>;
+
+
+  sortOption = [
+    {name: 'Alphabetical', value:'name'},
+    {name: 'Price: Low-High', value:'priceAsc'},
+    {name: 'Price: High-Low', value:'priceDesc'},
+
+  ];
+
+  shopParams = new ShopParams();
+  pageSizeOptions = [5, 10, 15, 20]
+
   ngOnInit(): void {
     this.initializeShop();
   }
@@ -34,35 +57,54 @@ export class ShopComponent implements OnInit {
   initializeShop(){
     this.shopService.getBrands();
     this.shopService.getTypes();
-    this.shopService.getProducts().subscribe({
-      next: response => this.products = response.data,
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => this.products = response,
       error:error => console.error(),
       complete: () => console.log('complete')
       
     })
+  }
+  onSearchChange(){
+    this.shopParams.pageNumber=1;
+    this.getProducts();
+  }
+
+  handlePageEvent(event: PageEvent){
+    this.shopParams.pageNumber = event.pageIndex + 1;
+    this.shopParams.pageSize = event.pageSize;
+    this.getProducts();
+  }
+
+  onSortChange(event: MatSelectionListChange){
+    const selectedOption = event.options[0];
+    if(selectedOption){
+      this.shopParams.sort = selectedOption.value;
+      this.shopParams.pageNumber = 1;
+      this.getProducts();
+
+    }
   }
 
   openFilterDialog(){
     const dialogRef = this.dialogService.open(FiltersDialogComponent, {
       minWidth: '500px',
       data: {
-        selectedBrands: this.selectedBrands,
-        selectedTypes: this.selectedTypes
+        selectedBrands: this.shopParams.brands,
+        selectedTypes: this.shopParams.types
       }
     });
     dialogRef.afterClosed().subscribe({
       next: result => {
         if(result){
           console.log(result);
-          this.selectedBrands = result.selectedBrands;
-          this.selectedTypes = result.selectedTypes;
+          this.shopParams.brands = result.selectedBrands;
+          this.shopParams.types = result.selectedTypes;
           
-          this.shopService.getProducts(this.selectedBrands, this.selectedTypes).subscribe(
-            {
-              next: response => this.products = response.data,
-              error: error => console.log(error)
-            }
-          );
+          this.getProducts();
         }
       }
     })
